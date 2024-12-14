55bb968e61658e17478fdcc3d06d869ebafe8178
rafal.witek <116377662+rwitek@users.noreply.github.com>
2024-12-14 17:21:04 +0000

    extended main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def dodaj(x,y):
-    return x+y
-    
-print (dodaj(2,1))
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s!' % name
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
